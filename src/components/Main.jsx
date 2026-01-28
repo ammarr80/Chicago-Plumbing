b4a51f634bee558f40f02ad6e75cbbd698e8a328
ammarr <ammarrusesthis80@gmail.com>
2026-01-28 07:41:15 +0500

    added Service Areas section

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,15 @@
 import Stats from "./Stats";
 import Services from "./Services";
 import WhyChooseUs from "./WhyChooseUs";
+import ServiceAreas from "./ServiceAreas";
 
 export default function Main() {
   return (
     <main className="space-y-26">
       <Stats />
       <Services />
-    <WhyChooseUs />
+      <WhyChooseUs />
+      <ServiceAreas />
     </main>
   );
 }
