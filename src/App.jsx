722a134cd88687227f22570d11462d73b70951e9
ammarr <ammarrusesthis80@gmail.com>
2026-02-01 09:42:41 +0500

    changed the import path for reactRouter

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,18 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 
-import { Routes, Route } from "react-router";
+import { Routes, Route, BrowserRouter } from "react-router";
 
-import HomePage from "./pages/HomePage";
-import ServicesPage from "./pages/ServicesPage";
+import HomePage from "./pages/HomePage/HomePage";
+import ServicesPage from "./pages/ServicesPage/ServicesPage";
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/services" element={<ServicesPage />} />
-      </Routes>
+     <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/services" element={<ServicesPage />} />
+    </Routes>
     </>
   );
 }
