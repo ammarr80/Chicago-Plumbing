15ecf0d557617064a409860a6cd9efa80f5eae49
ammarr <ammarrusesthis80@gmail.com>
2026-02-06 11:20:14 +0500

    added a route for LocationsPage

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,8 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import ServicesPage from "./pages/ServicesPage/ServicesPage";
 import ServicesInfo from "./pages/ServicesPage/components/ServicesInfo";
-
+import LocationsPage from "./pages/LocationsPage/LocationsPage";
+import LocationsInfo from "./pages/LocationsPage/components/LocationsInfo";
 function App() {
   return (
     <>
@@ -15,6 +16,8 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/services" element={<ServicesPage />} />
         <Route path="/services/:slug" element={<ServicesInfo />} />
+        <Route path="/locations" element={<LocationsPage />} />
+        <Route path="/locations/:slug" element={<LocationsInfo />} />
       </Routes>
     </>
   );
